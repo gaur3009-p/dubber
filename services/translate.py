2c0b382644cd044dd337fff696511812fbf9656e
gaur3009-p <gaur3009@gmail.com>
2026-03-06 01:41:39 +0530

    Refactor translate_text function for clarity

--- services/translate.py
+++ services/translate.py
@@ -5,7 +5,14 @@ model_name = "facebook/nllb-200-distilled-600M"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
-def translate_text(text, target_lang="eng_Latn"):
+
+def translate_text(text, target_lang):
+
     inputs = tokenizer(text, return_tensors="pt")
-    translated_tokens = model.generate(**inputs, forced_bos_token_id=tokenizer.lang_code_to_id[target_lang])
-    return tokenizer.decode(translated_tokens[0], skip_special_tokens=True)
+
+    tokens = model.generate(
+        **inputs,
+        forced_bos_token_id=tokenizer.lang_code_to_id[target_lang]
+    )
+
+    return tokenizer.decode(tokens[0], skip_special_tokens=True)
